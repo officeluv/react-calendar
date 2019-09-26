ed70c75e5e874818d3ba20f1a38a779f5106388d
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-09-26 15:03:13 +0200

    Create ESM builds

--- copy-types.js
+++ copy-types.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
-fs.copyFile('entry.nostyle.d.ts', 'dist/entry.nostyle.d.ts', (error) => {
+fs.copyFile('entry.nostyle.d.ts', 'dist/esm/entry.nostyle.d.ts', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('entry.nostyle.d.ts copied successfully.');
+  console.log('entry.nostyle.d.ts copied successfully to ESM build.');
+});
+
+fs.copyFile('entry.nostyle.d.ts', 'dist/umd/entry.nostyle.d.ts', (error) => {
+  if (error) {
+    throw error;
+  }
+  // eslint-disable-next-line no-console
+  console.log('entry.nostyle.d.ts copied successfully to UMD build.');
 });
